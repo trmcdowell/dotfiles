b8143103d0076a092aa5a9804b25c49ad8461f9f
Thomas McDowell <tmcdowell.dev@gmail.com>
2024-07-24 10:25:57 -0500

    Removed unnecessary plugin, added themes

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -12,6 +12,29 @@ return {
       },
     },
   },
+  -- add gruvbox
+  {
+    "ellisonleao/gruvbox.nvim",
+    lazy = false,
+    opts = {
+      transparent_mode = true,
+    },
+  },
+  -- add rosepine
+  {
+    "rose-pine/neovim",
+    name = "rose-pine",
+    lazy = false,
+    opts = {
+      variant = "main", -- auto, main, moon, or dawn
+      dark_variant = "main", -- main, moon, or dawn
+      styles = {
+        bold = true,
+        italic = false,
+        transparency = true,
+      },
+    },
+  },
   -- configure lazyvim to load tokyonight
   {
     "LazyVim/LazyVim",
